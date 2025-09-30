dad068741ccb32654e8abc58a5fc11193a6565bd
harshil2004788 <harshilprajapati651@gmail.com>
2025-09-30 18:15:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+    let tablinks = document.getElementsByClassName("tab-links");
+    let tabcontents = document.getElementsByClassName("tab-contents");
+
+    function opentab(tabname) {
+        for (let tablink of tablinks) {
+            tablink.classList.remove("active-link");
+        }
+        for (let tabcontent of tabcontents) {
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+    }
+
+    let sidemenu = document.getElementById('sidemenu');
+
+    function openmenu() {
+        sidemenu.style.right = "0";
+    }
+
+    function closemenu() {
+        sidemenu.style.right = "-200px"
+    }
+
+
+    const scriptURL = "https://script.google.com/macros/s/AKfycbzhF0DMxTud-haig_pqJYp2uRfuqby5VxVWX_ESHFdK9mIx7nPVHATl7YW4mCZED2k_4w/exec";
+    const form = document.forms["submit-to-google-sheet"];
+
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        fetch(scriptURL, {
+                method: "POST",
+                body: new FormData(form)
+            })
+            .then(response => console.log("Success!", response))
+            .catch(error => console.error("Error!", error.message));
+    });
